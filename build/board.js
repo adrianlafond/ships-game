d38656f0cfc98854e11f8546e2bf7ef3004d7135
Adrian Lafond <mail@adrianlafond.com>
2023-01-04 18:29:29 -0500

    render very basic ship

--- build/board.js
+++ build/board.js
@@ -1,3 +1,5 @@
+const BUTTON_BLOCK = 'ships__cell-button';
+const SHIP_BLOCK = 'ships__cell-ship';
 export class Board {
     constructor(rootElement) {
         this.rootElement = rootElement;
@@ -8,8 +10,23 @@ export class Board {
             this.renderBoard('defend', size)
         ].join('\n');
         // temp:
+        this.defendHit(1, 1);
+        this.defendMiss(1, 2);
         this.attackHit(1, 1);
         this.attackMiss(1, 2);
+        this.renderShip({
+            grid: 'attack',
+            ship: 'destroyer',
+            from: { row: 3, col: 1 },
+            direction: 'left'
+        });
+        this.renderShip({
+            grid: 'attack',
+            ship: 'destroyer',
+            from: { row: 5, col: 1 },
+            direction: 'down'
+        });
+        this.attackHit(3, 1);
     }
     renderBoard(type, size = 10) {
         let table = `<table class="ships__board ships__board--${type}">`;
@@ -17,7 +34,7 @@ export class Board {
         for (let i = 0; i < size; i++) {
             table += `<tr data-row="${i}" class="ships__board-row">`;
             for (let j = 0; j < size; j++) {
-                table += `<td data-col="${j}" class="ships__board-cell">`;
+                table += `<td data-col="${j}" class="ships__cell">`;
                 table += '<button class="ships__cell-button">';
                 table += '<span class="ships__cell-button-label">click</span>';
                 table += '</button>';
@@ -30,23 +47,57 @@ export class Board {
         return table;
     }
     defendMiss(row, col) {
-        //
+        this.markAttack(row, col, 'defend', 'miss');
     }
     defendHit(row, col) {
-        //
+        this.markAttack(row, col, 'defend', 'hit');
     }
     attackMiss(row, col) {
-        this.attack(row, col, 'attack', 'miss');
+        this.markAttack(row, col, 'attack', 'miss');
     }
     attackHit(row, col) {
-        this.attack(row, col, 'attack', 'hit');
+        this.markAttack(row, col, 'attack', 'hit');
     }
-    attack(row, col, grid, type) {
+    markAttack(row, col, grid, attackType) {
         const cell = document.querySelector(`table.ships__board--${grid} tr[data-row="${row}"] td[data-col="${col}"] button`);
         if (cell) {
-            const className = 'ships__cell-button';
-            cell.className = `${className} ${className}--${type}`;
+            const classList = this.removeAttackClassNames(cell);
+            classList.push(`${BUTTON_BLOCK}--${attackType}`);
+            cell.className = classList.join(' ');
         }
+    }
+    renderShip({ grid, ship, from, direction }) {
+        const cellFrom = document.querySelector(`table.ships__board--${grid} tr[data-row="${from.row}"] td[data-col="${from.col}"] button`);
+        if (direction === 'left') {
+            const cellTo = document.querySelector(`table.ships__board--${grid} tr[data-row="${from.row}"] td[data-col="${from.col + 2}"] button`);
+            if (cellTo) {
+                const classList = this.removeShipClassNames(cellTo);
+                classList.push(SHIP_BLOCK);
+                classList.push(`${SHIP_BLOCK}--right`);
+                cellTo.className = classList.join(' ');
+            }
+        }
+        if (cellFrom) {
+            const classList = this.removeShipClassNames(cellFrom);
+            classList.push(SHIP_BLOCK);
+            classList.push(`${SHIP_BLOCK}--left`);
+            cellFrom.className = classList.join(' ');
+        }
+        for (let i = from.col + 1; i < from.col + 2; i++) {
+            const cell = document.querySelector(`table.ships__board--${grid} tr[data-row="${from.row}"] td[data-col="${i}"] button`);
+            if (cell) {
+                const classList = this.removeShipClassNames(cell);
+                classList.push(SHIP_BLOCK);
+                classList.push(`${SHIP_BLOCK}--horizontal`);
+                cell.className = classList.join(' ');
+            }
+        }
+    }
+    removeAttackClassNames(element) {
+        return Array.from(element.classList).filter(item => item !== `${BUTTON_BLOCK}--hit` && item !== `${BUTTON_BLOCK}--miss`);
+    }
+    removeShipClassNames(element) {
+        return Array.from(element.classList).filter(item => !item.startsWith(SHIP_BLOCK));
     }
 }
 //# sourceMappingURL=board.js.map
--- src/board.ts
+++ src/board.ts
@@ -1,3 +1,14 @@
+export type Coord = {
+  row: number;
+  col: number;
+}
+
+const BUTTON_BLOCK = 'ships__cell-button';
+const SHIP_BLOCK = 'ships__cell-ship';
+
+export type Ship = 'patrol' | 'destroyer' | 'submarine' | 'battleship' | 'aircraft';
+export type Direction = 'left' | 'down' | 'right' | 'up';
+
 export class Board {
   constructor(private rootElement: HTMLElement) {}
 
@@ -8,8 +19,23 @@ export class Board {
     ].join('\n');
 
     // temp:
+    this.defendHit(1, 1);
+    this.defendMiss(1, 2);
     this.attackHit(1, 1);
     this.attackMiss(1, 2);
+    this.renderShip({
+      grid: 'attack',
+      ship: 'destroyer',
+      from: { row: 3, col: 1 },
+      direction: 'left'
+    });
+    this.renderShip({
+      grid: 'attack',
+      ship: 'destroyer',
+      from: { row: 5, col: 1 },
+      direction: 'down'
+    });
+    this.attackHit(3, 1);
   }
 
   private renderBoard(type: 'attack' | 'defend', size = 10) {
@@ -18,7 +44,7 @@ export class Board {
     for (let i = 0; i < size; i++) {
       table += `<tr data-row="${i}" class="ships__board-row">`;
       for (let j = 0; j < size; j++) {
-        table += `<td data-col="${j}" class="ships__board-cell">`
+        table += `<td data-col="${j}" class="ships__cell">`
         table += '<button class="ships__cell-button">'
         table += '<span class="ships__cell-button-label">click</span>'
         table += '</button>'
@@ -32,26 +58,70 @@ export class Board {
   }
 
   defendMiss(row: number, col: number) {
-    //
+    this.markAttack(row, col, 'defend', 'miss');
   }
 
   defendHit(row: number, col: number) {
-    //
+    this.markAttack(row, col, 'defend', 'hit');
   }
 
   attackMiss(row: number, col: number) {
-    this.attack(row, col, 'attack', 'miss');
+    this.markAttack(row, col, 'attack', 'miss');
   }
 
   attackHit(row: number, col: number) {
-    this.attack(row, col, 'attack', 'hit');
+    this.markAttack(row, col, 'attack', 'hit');
   }
 
-  private attack(row: number, col:number, grid: 'attack' | 'defend', type: 'hit' | 'miss') {
+  private markAttack(row: number, col:number, grid: 'attack' | 'defend', attackType: 'hit' | 'miss') {
     const cell = document.querySelector(`table.ships__board--${grid} tr[data-row="${row}"] td[data-col="${col}"] button`)
     if (cell) {
-      const className = 'ships__cell-button';
-      cell.className = `${className} ${className}--${type}`;
+      const classList = this.removeAttackClassNames(cell);
+      classList.push(`${BUTTON_BLOCK}--${attackType}`)
+      cell.className = classList.join(' ');
     }
+  }
+
+  renderShip({ grid, ship, from, direction }: {
+    grid: 'attack' | 'defend';
+    ship: Ship,
+    from: Coord;
+    direction: Direction;
+  }) {
+    const cellFrom = document.querySelector(`table.ships__board--${grid} tr[data-row="${from.row}"] td[data-col="${from.col}"] button`)
+    if (direction === 'left') {
+      const cellTo = document.querySelector(`table.ships__board--${grid} tr[data-row="${from.row}"] td[data-col="${from.col + 2}"] button`);
+      if (cellTo) {
+        const classList = this.removeShipClassNames(cellTo);
+        classList.push(SHIP_BLOCK);
+        classList.push(`${SHIP_BLOCK}--right`);
+        cellTo.className = classList.join(' ');
+      }
+    }
+
+    if (cellFrom) {
+      const classList = this.removeShipClassNames(cellFrom);
+      classList.push(SHIP_BLOCK);
+      classList.push(`${SHIP_BLOCK}--left`);
+      cellFrom.className = classList.join(' ');
+    }
+    for (let i = from.col + 1; i < from.col + 2; i++) {
+      const cell = document.querySelector(`table.ships__board--${grid} tr[data-row="${from.row}"] td[data-col="${i}"] button`);
+      if (cell) {
+        const classList = this.removeShipClassNames(cell);
+        classList.push(SHIP_BLOCK);
+        classList.push(`${SHIP_BLOCK}--horizontal`);
+        cell.className = classList.join(' ');
+      }
+    }
+  }
+
+  private removeAttackClassNames(element: Element) {
+    return Array.from(element.classList).filter(item =>
+      item !== `${BUTTON_BLOCK}--hit` && item !== `${BUTTON_BLOCK}--miss`);
+  }
+
+  private removeShipClassNames(element: Element) {
+    return Array.from(element.classList).filter(item => !item.startsWith(SHIP_BLOCK));
   }
 }
